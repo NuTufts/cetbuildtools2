1a15794e4c76b8c2b904f04a665d07cf0f4c33de
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2018-02-27 20:05:05 +0000

    Additional SIP tests/demos
    1. A PREBUILT script that is a shell script will fail if the
       interpreter is under SIP
    2. That the system dynamic loader path can be set programmatically,
       and by extension, extended.

---
+++ testing/testCetTestSIP/set_ldpath.cc
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+#ifdef __APPLE__
+  const char* name = "DYLD_LIBRARY_PATH";
+#else
+  const char* name = "LD_LIBRARY_PATH";
+#endif
+
+  const char* cetLDPathValue = getenv("CETD_LIBRARY_PATH");
+  int res = setenv("DYLD_LIBRARY_PATH", cetLDPathValue, 1);
+
+  const char* localLDPathValue = getenv(name);
+
+  if(strcmp(localLDPathValue,cetLDPathValue) != 0) {
+    return 1;
+  }
+  std::cout << localLDPathValue << std::endl;
+
+  return 0;
+}
